fa987568ca2499fc2c4be8c53d06dca4fde8d51a
Lavoisier Cornerstone <vois.cornerstone@gmail.com>
2014-01-14 14:42:51 -0500

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MapGame::Application.config.secret_token = '1a9ff77c28f3970f2d29a6518b7dd51c2932b82f027a1167a3dcd7864c762593a73eda245b557ac78e4b31528e975b872263537779f2f8f638d920ae1a89941a'
