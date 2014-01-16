b137637e6b531501b301b8c64a8d0bcb3f3f736d
Sean Reed <seanreed1111@gmail.com>
2014-01-16 15:18:48 -0500

    add GovJobs model

---
+++ db/migrate/20140116194720_create_gov_job.rb
@@ -0,0 +1,13 @@
+class CreateGovJob < ActiveRecord::Migration
+  def change
+    create_table :gov_jobs do |t|
+      t.string :work_location
+      t.integer :salary_range_to
+      t.integer :salary_range_from
+      t.integer :job_id
+      t.datetime :posting_date
+      t.float :latitude
+      t.float :longitude
+    end
+  end
+end 
