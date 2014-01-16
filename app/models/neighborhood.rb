0439238fdcf957798b42404361637513402c072b
ajosepha <ariel.isaacs@gmail.com>
2014-01-16 18:47:35 -0500

    modified models to be match our data

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,6 +1,5 @@
 class Neighborhood < ActiveRecord::Base
   has_many :gardens
-  has_many :laundromats
   has_many :licensed_businesses
   has_many :complaints
   has_many :farmers_markets
@@ -8,8 +7,6 @@ class Neighborhood < ActiveRecord::Base
   has_many :recycling_bins
   has_many :volunteer_opportunities
   has_many :gov_jobs
-  has_many :community_health_centersa
+  has_many :community_health_centers
   has_many :start_up_jobs
-
-
 end
