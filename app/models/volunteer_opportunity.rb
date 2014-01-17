ba6261eae4de8780f60e20856daf1deedfc38d51
Sean Reed <seanreed1111@gmail.com>
2014-01-17 17:44:53 -0500

    add volunteer_opportunity

--- app/models/volunteer_opportunity.rb
+++ app/models/volunteer_opportunity.rb
@@ -1,3 +1,21 @@
 class VolunteerOpportunity < ActiveRecord::Base
   belongs_to :neighborhood
+  attr_accessible :title, :latitude, :longitude, :human_address
+
+  def self.make_volunteer_opportunities
+    file = open('http://data.cityofnewyork.us/resource/bquu-z2ht.json')
+    parsed_file = JSON.parse(file.read)
+    parsed_file.each do |opportunity|
+      opportunity["locality"].each do |key, value|
+        if !VolunteerOpportunity.column_names.include?(key)
+          opportunity.delete(key)
+        else
+          opportunity[key] = value
+        end
+      end
+      opportunity.each do |key, value|
+        opportunity.delete(key) if !VolunteerOpportunity.column_names.include?(key)
+      end
+    VolunteerOpportunity.new(opportunity).save
+  end
 end
