f4ef81754e83d680c7c04d916f1c55d1aea585b0
ajosepha <ariel.isaacs@gmail.com>
2014-01-17 16:41:45 -0500

    add cultural organizations table

--- app/models/cultural_organization.rb
+++ app/models/cultural_organization.rb
@@ -1,3 +1,27 @@
+require 'open-uri'
+
 class CulturalOrganization < ActiveRecord::Base
+  attr_accessible :discipline_code, :longitude, :latitude, :human_address, :organization_name
+
   belongs_to :neighborhood
+
+  def self.make_cultural_organizations
+    file = open('http://data.cityofnewyork.us/resource/w6ua-xz2g.json')
+    parsed_file = JSON.parse(file.read)
+    parsed_file.each do |org|
+      org.each do |key, value|
+         org.delete(key) if !CulturalOrganization.column_names.include?(key)
+        # org.delete(key) if !CulturalOrganization.column_names.include?(key)
+        # if key == 'location_1' do |inner_key, inner_value|
+          
+        #end
+      #end
+    end
+  end
 end
+
+
+#rails generate migration CreateCulturalOrganization
+end
+
+
