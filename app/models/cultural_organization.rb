6ff882319103b21a8bb2c2639d148438d2644779
ajosepha <ariel.isaacs@gmail.com>
2014-01-20 11:07:33 -0500

    no real changes, just playing

--- app/models/cultural_organization.rb
+++ app/models/cultural_organization.rb
@@ -8,41 +8,34 @@ class CulturalOrganization < ActiveRecord::Base
   def self.make_cultural_organizations
     file = open('http://data.cityofnewyork.us/resource/w6ua-xz2g.json')
     parsed_file = JSON.parse(file.read)
-  end
 
 
-    # b = []
-    # #b is an array of hashes
-    # parsed_file.each do |org|
-    #   b << org["location_1"]
-    # end
+
+    b = []
+    #b is an array of hashes
+    parsed_file.each do |org|
+      b << org["location_1"]
+    end
+
+    #this works but gives me an error
+    b.each do |array|
+      array.delete("location_1")
+    end
 
     #  b.each do |array|
     #   array.each do |hash|
     #     hash.delete("needs_recoding")
     #   end
     # end
-    #   loc_hash.delete_if{ |key, value| key == "needs_recoding"}
-    # end
-  # end
-    #   #like calling first
-    #   array.each do |element|
-    #     element.delete("needs_recoding")
-    #   end
-    # end
-    #     element.each do |key, value|
-    #       element.delete(key) if key == "needs_recoding"
-    #     end
-    #  end
-    # end
-    #this works but throws an error into IRB
+  end
+
+end
+
 
         
 
 
 
 
-#rails generate migration CreateCulturalOrganization
-end
 
 
