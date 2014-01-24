264316859a429b85605e3771a7d8f60e2fbfe915
Sean Reed <seanreed1111@gmail.com>
2014-01-23 19:16:50 -0500

    adds find_community_health_centers

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,12 +2,16 @@ class Game < ActiveRecord::Base
   belongs_to :user
   belongs_to :zip
 
+  attr_reader :cultural_organizations, :community_health_centers
 
 
   def find_cultural_organizations
     @cultural_organizations = CulturalOrganization.where(:zip_code => self.zip.zip).to_a
   end
 
+  def find_community_health_centers
+    @community_health_centers = CommunityHealthCenter.where(:zip => self.zip.zip).to_a
+  end
 
 
 end
