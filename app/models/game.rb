f633f596c390d3c01ee07ddfca4b604de2431c49
Lavoisier Cornerstone <vois.cornerstone@gmail.com>
2014-01-29 11:18:06 -0500

    added attr accessible

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,9 @@
 class Game < ActiveRecord::Base
   belongs_to :user
-  has_many :game_zips
+  has_one :game_zips
 
-  attr_reader :cultural_organizations, :community_health_centers
+  attr_accessible :status_community_health_centers, :status_complaints, :status_cultural_organizations,
+  :status_gardens, :status_gov_jobs, :status_licensed_businesses, :status_recycling_bins, :status_restaurants, :status_senior_centers,
+  :status_start_up_jobs, :status_volunteer_opportunities, :status_youth_programs
 
 end
