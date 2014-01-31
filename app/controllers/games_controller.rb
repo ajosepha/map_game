79dc57394e99fbb08f03f45a55db825f9d96ddb3
Sean Reed <seanreed1111@gmail.com>
2014-01-31 11:53:35 -0500

    fix create form action

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,8 +10,12 @@ class GamesController < ApplicationController
   end
 
   def create
-    @game = Game.new(:user_id => params[:user_id],:game_zip_id => GameZip.where(:zip => params[:game_zip]))
-    render show
+    @user = User.find(params[:user_id])
+    @game = Game.new(:user_id => params[:user_id],
+            :game_zip_id => GameZip.where(:zip => params[:game_zip]).first.id)
+    @game.save
+ 
+    render "show"
   end
 
 end
