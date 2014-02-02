002b9cd6f20a41efe31052fca3cbba904b042087
Sterling Walker <lazarus_star@hotmail.com>
2014-02-02 15:35:59 -0500

    New game action checks against duplicate games.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,11 +11,19 @@ class GamesController < ApplicationController
 
   def create
     @user = User.find(params[:user_id])
-    @game = Game.new(:user_id => params[:user_id],
-            :game_zip_id => GameZip.where(:zip => params[:game_zip]).first.id)
-    @game.save
- 
-    redirect_to action: 'show', status: 302
+    @repeat_flag = false
+    @user.games.each do |existing_game|
+      if existing_game.game_zip_id == GameZip.where(:zip => params[:game_zip]).first.id
+        @repeat_flag = true
+        @game = existing_game
+      end
+    end
+    unless @repeat_flag
+      @game = Game.new(:user_id => params[:user_id], :game_zip_id => GameZip.where(:zip => params[:game_zip]).first.id)
+      @game.save
+    end
+      redirect_to(action: 'show', id: @game.id, status: 302)
   end
+
 
 end
