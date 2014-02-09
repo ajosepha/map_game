e13843f7d91b4649dd5d49829d396611b97c8d4f
ajosepha <ariel.isaacs@gmail.com>
2014-02-09 18:22:29 -0500

    add bacgkround images to game

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def randomized_background_image
+    images =["assets/bridge.jpg", "assets/chrystler.jpg", "assets/coney-island
+      .jpg", "assets/coney2.jpg", "assets/dumbo.jpg", "assets/grand_central.jpg", "assets/life.jpg",
+      "assets/life.jpg", "assets/lower.jpg"]
+    images[rand(images.size)]
+  end
 end
