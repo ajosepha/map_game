270a0e8c9ef8b212161e746f7ebd86ed37622816
ajosepha <ariel.isaacs@gmail.com>
2014-02-12 11:41:41 -0500

    add links to header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,3 @@
 module ApplicationHelper
-  def randomized_background_image
-    images =["assets/bridge.jpg", "assets/chrystler.jpg", "assets/coney-island
-      .jpg", "assets/coney2.jpg", "assets/dumbo.jpg", "assets/grand_central.jpg", "assets/life.jpg",
-      "assets/life.jpg", "assets/lower.jpg"]
-    images[rand(images.length)]
-  end
+ 
 end
