1ddf5cb2c2f82b6b83a2f167b0b23b2a761da0c5
ajosepha <ariel.isaacs@gmail.com>
2014-02-18 22:02:51 -0500

    add jquery to picking a game zip

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
+  var $this = $(this);
 
-  $(".Borough").hide()
-})
+  $(".borough").hide();
+
+  $(".hood").on("click", function(event){
+    alert("test");
+    $(this).children().show("slow", function(){
+
+    });
+    //$(".Borough").show("slow", function(){
+
+    //});
+  });
+});
+
