1a563599190c72a5eae3a1564d8276a54c267da6
ajosepha <ariel.isaacs@gmail.com>
2014-02-20 09:05:21 -0500

    add session

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 MapGame::Application.routes.draw do
 
 resources :users do
+  resources :sessions, only: [:new, :create, :destroy]
+
   resources :games do
     post '/purchase', to: "games#purchase"
   end
 end
-#root : 
+
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
+
 resources :game_zips 
 end
