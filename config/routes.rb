2df697f0fed30dbb411f7a0b108c9ca57fa77b73
ajosepha <ariel.isaacs@gmail.com>
2014-02-23 18:42:49 -0500

    add flash message to sign in

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,16 @@ root :to => "users#index"
 resources :users do
   match '/signup',  to: 'users#new',            via: 'get'
   resources :sessions, only: [:new, :create, :destroy]
+    match '/signup',  to: 'users#new',            via: 'get'
+    match '/signin',  to: 'sessions#new',         via: 'get'
+    match '/signout', to: 'sessions#destroy',     via: 'delete'
 
   resources :games do
     post '/purchase', to: "games#purchase"
   end
 end
 
-  match '/signup',  to: 'users#new',            via: 'get'
-  match '/signin',  to: 'sessions#new',         via: 'get'
-  match '/signout', to: 'sessions#destroy',     via: 'delete'
+
 
 resources :game_zips 
 end
