6b6144d263c9ca83715b23afa409cd8bbe8cbf9a
ajosepha <ariel.isaacs@gmail.com>
2014-02-23 18:22:26 -0500

    add user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,30 @@
 class UsersController < ApplicationController
   protect_from_forgery
 
-  def new
-    @user = User.new
-  end
+ 
 
   # def game
   #   @game = user.game
   #   @zip = user.game.zip
   # end
 
-  # def create
-  #   @user = User.new(params[:user])
-  #   if @user.save
-  #     redirect_to @user
-  #   else
-  #     render 'new'
-  #   end
-  # end
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
 
-  # def show
-  #   @user = User.find(params[:id])
-  # end
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
 
   # def index
   # end
