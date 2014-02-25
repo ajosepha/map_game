03256f130dae5b0c9cf97c3c7c03dfd584c094fa
ajosepha <ariel.isaacs@gmail.com>
2014-02-25 08:14:00 -0500

    add signing form and link

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate[:password]
+      session[:user_id] = user.id
+        redirect_to root_url, notice: "Logged in!"
+        #above will need to be changed, this is a test
+    else
+      flash.now.alert = "email or password is not valid"
+      render "new"
+    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ MapGame::Application.routes.draw do
 
 root :to => "users#index"
 resources :users do
-resources :sessions, only: [:new, :create, :destroy] 
-    match '/signup',  to: 'users#new'          
-    match '/signin',  to: 'sessions#new'     
-   match '/signout', to: 'sessions#destroy',     via: 'delete'
+resources :sessions 
+#only: [:new, :create, :destroy] 
+#     match '/signup',  to: 'users#new'          
+#     match '/signin',  to: 'sessions#new'     
+#    match '/signout', to: 'sessions#destroy',     via: 'delete'
 
   resources :games do
     post '/purchase', to: "games#purchase"
