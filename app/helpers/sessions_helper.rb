6b767df25ed4e705f0908e376af945f712bdc1ac
ajosepha <ariel.isaacs@gmail.com>
2014-02-27 18:40:35 -0500

    add sign in and out, still need to redo roots

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,5 +17,10 @@ module SessionsHelper
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
 
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
 
 end
