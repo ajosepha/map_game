18b60c24964d4fb9f147708cf663aae44c3e02ec
ajosepha <ariel.isaacs@gmail.com>
2014-02-27 18:06:22 -0500

    add sign in form, take out game zips from header

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,20 @@
 MapGame::Application.routes.draw do
 
 root :to => "users#index"
-resources :users do
-resources :sessions 
-#only: [:new, :create, :destroy] 
-#     match '/signup',  to: 'users#new'          
-#     match '/signin',  to: 'sessions#new'     
-#    match '/signout', to: 'sessions#destroy',     via: 'delete'
+resources :users #do
+resources :sessions, only: [:new, :create, :destroy]
 
-  resources :games do
-    post '/purchase', to: "games#purchase"
-  end
-end
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
 
+#   resources :games do
+#     post '/purchase', to: "games#purchase"
+#   end
+# end
 
-resources :game_zips 
+
+
+# resources :game_zips 
 end
