8194082dbe26dfcec7394a84304d1f312cd5e015
ajosepha <ariel.isaacs@gmail.com>
2014-02-28 15:14:28 -0500

    remove sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ MapGame::Application.routes.draw do
 
 root :to => "users#index"
 resources :users do
-resources :sessions 
+#resources :sessions 
 #only: [:new, :create, :destroy] 
 #     match '/signup',  to: 'users#new'          
 #     match '/signin',  to: 'sessions#new'     
