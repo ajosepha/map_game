041782759670e9144b9a0bd1c5beb716f76d3778
ajosepha <ariel.isaacs@gmail.com>
2014-02-28 06:36:13 -0500

    get user tests to pass with remember token

--- db/migrate/20140222233150_remove_twitter_from_users.rb
+++
@@ -1,9 +0,0 @@
-class RemoveTwitterFromUsers < ActiveRecord::Migration
-  def up
-    remove_column :users, :twitter
-  end
-
-  def down
-    add_column :users, :twitter, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -85,8 +85,6 @@ ActiveRecord::Schema.define(:version => 20140227231052) do
     t.float    "size"
     t.float    "latitude"
     t.float    "longitude"
-    t.integer  "zip"
-    t.integer  "game_zip_id"
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
   end
@@ -167,9 +165,11 @@ ActiveRecord::Schema.define(:version => 20140227231052) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
   create_table "volunteer_opportunities", :force => true do |t|
     t.string   "title"
