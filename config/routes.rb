91ebd28f11d62d7f98ba8df1dc79a563da10be14
Sterling Walker <lazarus_star@hotmail.com>
2014-03-01 23:32:24 -0500

    Remove link in user show page that is causing routing problems

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 MapGame::Application.routes.draw do
 
 root :to => 'users#index'
+
+resources :sessions, only: [:new, :create, :destroy]
+
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+
+get 'users', to: 'users#show'
+
 resources :users do
   resources :games do
     post '/purchase', to: 'games#purchase'
@@ -8,11 +17,5 @@ resources :users do
 end
 
 resources :game_zips, only: [:show, :index]
-
-resources :sessions, only: [:new, :create, :destroy]
-
-  match '/signup',  to: 'users#new'
-  match '/signin',  to: 'sessions#new'
-  match '/signout', to: 'sessions#destroy', via: :delete
 
 end
