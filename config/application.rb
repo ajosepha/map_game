c11705ab20af9fc9bfb97162ba99368823616908
ajosepha <ariel.isaacs@gmail.com>
2014-03-03 13:14:08 -0500

    change assets on precomile

--- config/application.rb
+++ config/application.rb
@@ -52,6 +52,8 @@ module MapGame
 
     I18n.enforce_available_locales = true
 
+    config.assets.initialize_on_precompile = false
+
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
