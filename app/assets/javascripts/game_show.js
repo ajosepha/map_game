e9061061ea00b97b52951a95f20819be70da2d6a
ajosepha <ariel.isaacs@gmail.com>
2014-03-05 14:49:43 -0500

    add toggle class to game

--- app/assets/javascripts/game_show.js
+++ app/assets/javascripts/game_show.js
@@ -2,11 +2,16 @@ $(document).ready(function(){
 
   var $this = $(this);
 
-  $(".elements").hide();
+  $(".elements").addClass("hidden");
 
   $(".card").on("click", function(event){
-    $(this).find(".elements").show();
+    $(this).find(".elements").toggleClass("hidden");
   })
 
   $(".status:contains('Congrats!')").css("background-color", "#57FF97")
 });
+
+$(".things").addClass("hidden");
+  $(".card").on("click", function(event){
+    $(this).find(".things").toggleClass("hidden");
+  });
